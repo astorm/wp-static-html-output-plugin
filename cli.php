179d949568fcc5b971409ebc884a51e0cd6b9634
Alan Storm <astorm@alanstorm.com>
2017-10-21 00:11:19 -0700

    Print out instructions

--- cli.php
+++ cli.php
@@ -55,6 +55,12 @@ class WPStaticHtmlOutput_Command extends WP_CLI_Command {
             WP_CLI::success($path);                
         }        
         WP_CLI::success( "Done" );
+        
+        $path = trim($path,'/');
+        if(file_exists(__DIR__ . '/' . 'instructions.txt'))
+        {
+            WP_CLI::success(file_get_contents(__DIR__ . '/' . 'instructions.txt'));    
+        }
     }
 }
 
